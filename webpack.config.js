8622f2b3fe0d64e6632e4897a271714b8b574c5f
Jake Degiovanni <degiojake@gmail.com>
2018-06-23 21:19:15 +0100

    Added React and webpack hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './src/index.ts'
+        app: ['./src/app/App.tsx', 'webpack-hot-middleware/Client'],
+        vendor: ['react', 'react-dom']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
+        filename: 'js/[name].bundle.js'
     },
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
     },
+    devtool: "source-map",
     module: {
         rules: [
             {
-                test: /\.ts?$/,
+                test: /\.(ts|tsx)$/,
                 use: "ts-loader"
+            },
+            {
+                enforce: "pre", test: /\.js$/, loader: "source-map-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'app', 'index.html')}),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };

c65f99b894d671180dd01235d3d81d29c0f17713
Jake Degiovanni <degiojake@gmail.com>
2018-06-23 22:07:28 +0100

    Added Hello html to a prvate class element and typed it

--- src/app/components/Hello.tsx
+++ src/app/components/Hello.tsx
@@ -7,7 +7,13 @@ interface IProps {
 }
 
 export class Hello extends React.Component<IProps, {}> {
+    private element: JSX.Element = (
+        <h1>
+            This is a {this.props.framework} application using {this.props.compiler} with {this.props.bundler}.
+        </h1>
+    );
+
     render() {
-        return <h1>This is a {this.props.framework} application using {this.props.compiler} with {this.props.bundler}.</h1>;
+        return this.element;
     }
 }

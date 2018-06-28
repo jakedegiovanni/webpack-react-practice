3b3b5ed7fd70a3e00a6b44c43971c4c148e46035
Jake Degiovanni <degiojake@gmail.com>
2018-06-28 15:55:16 +0100

    Finished basic react demo, this will all be replaced with a small app

--- src/app/components/Hello.tsx
+++ src/app/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components"
 
 interface IProps {
     compiler: string;
@@ -6,14 +7,70 @@ interface IProps {
     bundler: string;
 }
 
-export class Hello extends React.Component<IProps, {}> {
-    private element: JSX.Element = (
-        <h1>
-            This is a {this.props.framework} application using {this.props.compiler} with {this.props.bundler}.
-        </h1>
-    );
+type IState = IProps;
+
+export class Hello extends React.Component<IProps, Partial<IState>> {
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+            compiler: props.compiler,
+            framework: props.framework,
+            bundler: props.bundler
+        };
+    }
+
+    private updateElement = (event: React.FormEvent<HTMLInputElement>): void => {
+        const target: HTMLInputElement = event.target as HTMLInputElement;
+        const name = target.name;
+
+        this.setState({
+            [name]: target.value
+        });
+    }
 
     render() {
-        return this.element;
+        const InputBox = styled.div`
+            display: flex;
+        `;
+
+        const InputItem = styled.div`
+            flex: 1;
+        `;
+
+        const InputText = styled.input`
+            margin-left: 8px;
+        `;
+
+        return (
+            <div>
+                <div>
+                    <h1>
+                        This is a {this.state.framework} application using {this.state.compiler} with {this.state.bundler}.
+                    </h1>
+                </div>
+                <InputBox>
+                    <InputItem>
+                        <label>
+                            Framework: 
+                            <InputText name="framework" value={this.state.framework} onChange={this.updateElement} />
+                        </label>
+                    </InputItem>
+                    <InputItem>
+                        <label>
+                            Compiler: 
+                            <InputText name="compiler" value={this.state.compiler} onChange={this.updateElement} />
+                        </label>
+                    </InputItem>
+                    <InputItem>
+                        <label>
+                            Bundler: 
+                            <InputText name="bundler" value={this.state.bundler} onChange={this.updateElement} />
+                        </label>
+                    </InputItem>
+                </InputBox>
+            </div>
+        );
     }
 }
